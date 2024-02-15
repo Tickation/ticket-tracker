c051cd19dfff83115ca0ec0f5086907f42616aee
tablecleaner88 <tablecleaner88@gmail.com>
2024-02-15 15:58:06 +0800

    :sparkles: Add wagmi setup and icons

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { wagmiConfig } from './constants';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient() 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}> 
+          <App />
+        </QueryClientProvider> 
+    </WagmiProvider>
   </React.StrictMode>,
 )
